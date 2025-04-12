0dc153b3016f3177bb2bbc4c8834a77e776623db
beccaenz <beccaenzinna.5@gmail.com>
2025-04-12 12:38:29 -0600

    added transition for form

--- scripts/index.js
+++ scripts/index.js
@@ -15,16 +15,102 @@ console.log(initialCards);
 
 
 const profileEditButton = document.querySelector(".profile__edit-button");
+const editModalCard = document.querySelector("#edit-card-modal");
 const editModal = document.querySelector("#edit-profile-modal");
 const closeButton = editModal.querySelector(".modal__close-btn");
+const closeButtonCard = editModalCard.querySelector(".modal__close-btn");
+const saveModal = editModal.querySelector(".modal__submit-btn");
+const nameInput = editModal.querySelector("#name");
+const descriptionInput = editModal.querySelector("#description");
+const profileName = document.querySelector(".profile__name");
+const profileDecription = document.querySelector(".porfile__description");
+const addCardFormElement = document.querySelector(".profile__add-button"); 
+const modalSubmitCard = editModalCard.querySelector(".modal__submit-btn"); // wrong selectorr?? no log to console
+const nameInputCard = editModalCard.querySelector("#link"); 
+const linkInput = editModalCard.querySelector("#caption");
+
+
+
+
+
+function saveProfile(event){
+   
+   
+   profileName.innerHTML = nameInput.value;
+    profileDecription.innerHTML = descriptionInput.value;
+
+
+    console.log(nameInput.value);
+
+    editModal.classList.remove("modal_opened");
+    event.preventDefault();
+}
+
 
 function openModal(){
     editModal.classList.add("modal_opened");
+
+
+    // change the placeholder to current profile name
+    nameInput.value =profileName.textContent; 
+    descriptionInput.value = profileDecription.textContent;
+
+
 }
 
 function closeModal(){
     editModal.classList.remove("modal_opened");
 }
 
-profileEditButton.addEventListener("click", openModal);
+
+function openModalCard(){
+    editModalCard.classList.add("modal_opened");
+
+
+
+
+}
+
+function closeModalCard(){
+    editModalCard.classList.remove("modal_opened");
+
+
+
+}
+
+
+
+
+
+
+addCardFormElement.addEventListener("click", openModalCard); // correct??
+closeButtonCard.addEventListener("click", closeModalCard);
 closeButton.addEventListener("click", closeModal);
+saveModal.addEventListener("click", saveProfile);
+profileEditButton.addEventListener("click", openModal);
+
+
+
+
+
+
+
+
+// Create the form submission handler.
+function handleAddCardSubmit(evt) {
+  // Prevent default browser behavior.
+  evt.preventDefault(); 
+ 
+  // Log both input values to the console.
+
+  console.log(nameInputCard.value);
+  console.log(linkInput.value);
+
+  // Close the modal.
+  editModalCard.classList.remove("modal_opened");
+
+
+}
+
+// Create the submit listener.
+modalSubmitCard.addEventListener('click', handleAddCardSubmit);
